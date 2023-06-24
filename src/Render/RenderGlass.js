33fb4218d9b4a296afcb5d83a7804818e5547abe
dovanrin <vanrinrt@gmail.com>
2023-06-24 20:51:27 +0700

    nop bai

---
+++ src/Render/RenderGlass.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { arrGlass } from "./Databyding";
+
+export default class RenderGlass extends Component {
+  state = {
+    glass: "./img_ex/glassesImage/v1.png",
+  };
+
+  renderListGass = () => {
+    return arrGlass.map(({ url, name, hinhModel }, index) => {
+      return (
+        <div
+          key={index}
+          class="card text-start col-1 m-2 align-items-center"
+          onClick={() => {
+            this.handelGlass(url);
+          }}
+        >
+          <img class="card-img-top p-2 w-100" src={hinhModel} alt="Title" />
+        </div>
+      );
+    });
+  };
+  handelGlass = (value) => {
+    this.setState({ glass: value });
+  };
+  render() {
+    let { glass } = this.state;
+    console.log(glass);
+    return (
+      <div>
+        <div className="w-75 text-center container position-relative abs">
+          <img className="w-50" src="./img_ex/glassesImage/model.jpg" alt="" />
+          <img className="position-absolute glassMOdel" src={glass} alt="" />
+        </div>
+        <div className="row">{this.renderListGass()}</div>
+      </div>
+    );
+  }
+}
